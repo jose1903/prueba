83f0c69e70f9948090c499e1d62f6e8ee489d060
jose1903 <32714300+jose1903@users.noreply.github.com>
2017-10-11 16:19:32 -0500

    primer cambio
    pirmer cambio

---
+++ Archivos/src/archivos/Archivos.java
@@ -0,0 +1,58 @@
+
+package archivos;
+import java.io.File; 
+import java.io.IOException; 
+import javax.swing.JOptionPane;
+
+public class Archivos 
+{
+    
+ public static int menu()
+    { 
+      int opcion = 0;
+
+      do{
+          opcion = Integer.parseInt(JOptionPane.showInputDialog("=== IMPLEMENTACIÓN DE ARCHIVOS - OPCIÓN DEL MENÚ ===\n\n" +"1. Guardar Nombres en el Archivo \n"+"2. Listar los Nombres del Archivo \n"+"3. Cantidad de Nombres Guardados\n"+"4. Informacion General del Archivo \n"+"5. Salir"+"\n \n Seleccione una opcion del 1 al 5"));
+        }
+        while(opcion <= 0 || opcion > 5); 
+        return opcion; 
+    }
+ public static void informeArchivo(LeerEscribirArchivo arch)
+    {
+      File inf = new File(arch.getRutaArchivo());
+      JOptionPane.showMessageDialog(null,"====== INFORMACÓN DEL ARCHIVO ======"+"\n"+"\n Ubicación: "+ inf.getAbsolutePath()+"\n Tamaño: "+ inf.length());   
+    }
+ public static void main(String[] args) throws IOException
+    {
+      int opcion; 
+      LeerEscribirArchivo arch = new LeerEscribirArchivo();
+      arch.setRutaArchivo("C:\\personas.txt");
+      do
+        {
+          opcion = menu();
+          
+          switch(opcion) 
+            {
+              case 1: String dato = JOptionPane.showInputDialog(null, "Digite el NOMBRE: ");
+                  arch.guardarArchivo(dato);
+              break;
+              case 2: 
+                 JOptionPane.showMessageDialog(null,"====== NOMBRES DEL ARCHIVO ======"+"\n"+ arch.leerArchivo(arch.getRutaArchivo())+"\n"); 
+              break;
+              case 3: 
+                 JOptionPane.showMessageDialog(null,"====== NOMBRES DEL ARCHIVO ======"+"\n"+"\n"+"Se han registrado: "+ arch.cantidadLineasArchivo()+" Nombres \n\n"); 
+              break;
+              case 4: 
+                 informeArchivo(arch);
+              break;
+              case 5:
+              break; 
+            }
+        }while(opcion != 5); 
+     }
+}    
+   
+    
+
+
+
---
+++ Archivos/src/archivos/LeerEscribirArchivo.java
@@ -0,0 +1,81 @@
+
+package archivos;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class LeerEscribirArchivo
+{
+    
+    private String rutaArchivo;
+    File archivo;
+    public void LeerEscribirArchivo()
+    { 
+        rutaArchivo = "";
+    }    
+    public void setRutaArchivo(String nom)  
+    { 
+        rutaArchivo = nom;
+    } 
+    
+    
+    public String getRutaArchivo() { 
+       return rutaArchivo;
+    }
+     //otro cambio
+    public void guardarArchivo(String dato) throws IOException
+    { 
+      archivo = new File(getRutaArchivo());
+      BufferedWriter escribir;
+      if(archivo.exists())
+        {  
+         escribir = new BufferedWriter(new FileWriter(archivo, true)); 
+         escribir.write(dato);
+         escribir.newLine();
+        } else
+        { 
+           escribir = new BufferedWriter(new FileWriter(archivo)); 
+           escribir.write(dato);
+           escribir.newLine();
+        }
+      escribir.close(); 
+    }
+    public String leerArchivo(String rutaArchivo) throws FileNotFoundException, IOException 
+    { 
+           
+       String linea;
+       String verDatosArchivo="";
+       FileReader fichero = new FileReader(rutaArchivo); 
+       BufferedReader leer = new BufferedReader(fichero);
+       while((linea = leer.readLine())!= null)
+        { 
+            verDatosArchivo = verDatosArchivo+String.valueOf(linea+"\n");
+        }
+        leer.close();
+        return verDatosArchivo; 
+    }
+    public int cantidadLineasArchivo() throws FileNotFoundException, IOException
+    {
+        int numeroLineas = 0;
+        String linea;
+        FileReader fichero = new FileReader(getRutaArchivo()); 
+        BufferedReader leer = new BufferedReader(fichero);
+        while((linea = leer.readLine())!= null) 
+        {
+            numeroLineas++; 
+        } 
+        leer.close();
+        return numeroLineas; 
+    }
+}
+
+
+
+    
+
+
